90289a986a6f36776508e15874ceb4bccfaa69fb
TFuray <pk.master881@gmail.com>
2023-01-09 20:11:54 -0800

    added frontend ui

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,21 +1,38 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-  user: user? user: null,
+  user: user ? user : null,
   isError: false,
   isSuccess: false,
   isLoading: false,
   message: ''
 }
 
+// Register user
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user)
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    reset: () => {
+    reset: state => {
       state.isLoading = false
       state.isSuccess = false
 
@@ -23,8 +40,8 @@ export const authSlice = createSlice({
       state.message = ''
     }
   },
-  extraReducers: () =>{}
+  extraReducers: () => {}
 })
 
-export const {reset} = authSlice.actions
+export const { reset } = authSlice.actions
 export default authSlice.reducer
