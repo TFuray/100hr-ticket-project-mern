19294b2c1a4d9839086cc432d22f54d4601facee
TFuray <pk.master881@gmail.com>
2023-01-16 12:13:49 -0800

    working on showing tickets

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,37 @@
+/* eslint-disable no-unused-vars */
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import TicketForm from '../components/TicketForm'
+import Spinner from '../components/Spinner'
+import { getTickets, reset } from '../features/tickets/ticketSlice'
 
 const Dashboard = () => {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const { user } = useSelector(state => state.auth)
+  const { tickets, isLoading, isError, message } = useSelector(
+    state => state.tickets
+  )
 
   useEffect(() => {
+    if (isError) {
+      console.log(message)
+    }
     if (!user) {
       navigate('/login')
     }
-  }, [user, navigate])
+    dispatch(getTickets())
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, isError, message, dispatch])
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
